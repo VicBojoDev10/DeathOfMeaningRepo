7f10f1890e7f6443be3751b7c5fe9a309a151e1b
Angry6irds <speedlimon34@gmail.com>
2026-08-26 20:22:15 -0600

    feat: GravityModel  POCO agregado

---
+++ The_Death_Of_Meaning/Assets/ALEJANDRO/SCRIPTS/POCO/GravityModel.cs
@@ -0,0 +1,8 @@
+namespace ALEJANDRO.SCRIPTS.POCO
+{
+    public sealed class GravityModel
+    {
+        private readonly float _gravity;
+        private readonly float _terminalVelocity;
+    }
+}
