506425b60567739fde265688d62f3f16108ce0e1
Misael Bautista <misagamer559@gmail.com>
2026-08-28 22:20:02 -0600

    feat: Adding the initial set up for networking system

---
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/Unity/Network/ClientPlayerMove.cs
@@ -0,0 +1,30 @@
+using System;
+using StarterAssets;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClientPlayerMove : NetworkBehaviour
+{
+    [SerializeField] private PlayerInput m_playerInput;
+    [SerializeField] private StarterAssetsInputs m_StarterAssetsInputs;
+    [SerializeField] private ThirdPersonController m_ThirdPersonController;
+
+    private void Awake()
+    {
+        m_StarterAssetsInputs.enabled = false;
+        m_playerInput.enabled = false;
+        m_ThirdPersonController.enabled = false;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+     base.OnNetworkSpawn();
+     if (IsOwner)
+     {
+         m_StarterAssetsInputs.enabled = true;
+         m_playerInput.enabled = true;
+         m_ThirdPersonController.enabled = true;
+     }
+    }
+}
