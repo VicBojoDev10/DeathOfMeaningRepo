5e5e45125fd2e4ad238af5fdfba6495131515d70
Angry6irds <speedlimon34@gmail.com>
2026-08-29 20:44:59 -0600

    feat: tests and scripts functionable

---
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/Contracts/InputSnapshot.cs
@@ -0,0 +1,8 @@
+﻿namespace TDOM.Contracts
+{
+    public class InputSnapshot
+    {
+        public bool JumpPressed;
+        public bool JumpHeld;
+    }
+}
---
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/Contracts/LocomotionState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace TDOM.Contracts
+{
+    public class LocomotionState
+    {
+        public bool IsGrounded;
+        public Vector3 Velocity;
+        public int JumpsUsed;
+        public float CoyoteTimer;
+        public float BufferTimer;
+    }
+}
