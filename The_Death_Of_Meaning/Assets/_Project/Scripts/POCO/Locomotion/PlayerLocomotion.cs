299430b758861256bf04d3a7cb45550cae43efa8
VicBojo <reybojorgejr@hotmail.com>
2026-08-30 22:07:51 -0600

    feat: Player Locomotion & Motor Scripts created

--- The_Death_Of_Meaning/Assets/_Project/Scripts/POCO/Locomotion/PlayerLocomotion.cs
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/POCO/Locomotion/PlayerLocomotion.cs
@@ -1,34 +1,3 @@
-/*using UnityEngine;
+using UnityEngine;
 
-public sealed class PlayerLocomotion
-{
-    private readonly GravityModel _gravity;
-    private readonly JumpResolver _jump;
-    private readonly GroundControlResolver _ground;
-    private readonly SprintResolver _run;
-    private readonly DashResolver _dash;
-
-    public LocomotionState State { get; } = new();
-
-    public MotionIntent Tick(InputSnapshot input, Quaternion yaw, float dt)
-    {
-        _correr.Tick(input);
-
-        if (input.DashPressed)
-        {
-            Vector3 dir = DashDirection(input, yaw);
-            _dash.TryStart(dir);
-        }
-
-        _dash.Tick(State, input.Move, dt);
-
-        if(_dash.Activo)
-            return new MotionIntent(State.Velocity, ignoreGravity: true);
-
-        _jump.TIck(State, input, dt);
-        _jump.TIck(State, input.Move, yaw, _run.Corriendo, dt);
-        _gravity.Aplicar(State, input, dt);
-
-        return new MotionIntent(State.Velocity, ignoreGravity: false);
-    }
-}*/
+public sealed class PlayerLocomotion { }
--- The_Death_Of_Meaning/Assets/_Project/Scripts/Unity/Player/PlayerMotor.cs
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/Unity/Player/PlayerMotor.cs
@@ -1,16 +1,3 @@
-/*using UnityEngine;
+using UnityEngine;
 
-public sealed class PlayerMotor : MonoBehaviour
-{
-    [SerializeField] private CharacterController _cc;
-
-    public void Apply(MotionIntent intent, float dt)
-    {
-        _cc.Move(intent.Velocity * dt);
-    }
-
-    public void ProbeGround(LocomotionState state)
-    {
-        state.IsGrounded = _cc.isGrounded;
-    }
-}*/
+public sealed class PlayerMotor : MonoBehaviour { }
