35a2da5a69b3861e5c0d2699ab2d35b685c6cd01
Angry6irds <speedlimon34@gmail.com>
2026-09-03 00:36:14 -0600

    fix: fixing csharpier again

--- The_Death_Of_Meaning/Assets/_Project/Tests/EditMode/SprintResolverTest.cs
+++ The_Death_Of_Meaning/Assets/_Project/Tests/EditMode/SprintResolverTest.cs
@@ -7,30 +7,24 @@ namespace TDOM.Tests
 {
     public sealed class SprintResolverTest
     {
+        [Test]
+        public void Correr_se_apaga_al_soltar_el_stick()
+        {
+            var resolver = new SprintResolver();
 
-            [Test]
-            public void Correr_se_apaga_al_soltar_el_stick()
-            {
-                var resolver = new SprintResolver();
+            var input = new InputSnapshot(false, false) { SprintPressed = true, Move = Vector2.up };
 
-                var input = new InputSnapshot(false, false)
-                {
-                    SprintPressed = true,
-                    Move = Vector2.up,
-                };
+            resolver.Tick(input);
+            Assert.IsTrue(resolver.Corriendo);
 
-                resolver.Tick(input);
-                Assert.IsTrue(resolver.Corriendo);
+            input.SprintPressed = false;
+            resolver.Tick(input);
+            Assert.IsTrue(resolver.Corriendo);
 
-                input.SprintPressed = false;
-                resolver.Tick(input);
-                Assert.IsTrue(resolver.Corriendo);
+            input.Move = Vector2.zero;
+            resolver.Tick(input);
 
-                input.Move = Vector2.zero;
-                resolver.Tick(input);
-
-                Assert.IsFalse(resolver.Corriendo);
-            }
-        
+            Assert.IsFalse(resolver.Corriendo);
+        }
     }
 }
