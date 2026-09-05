1ccb9f9dea58301ca41402fd70acd698089bc9e2
Angry6irds <speedlimon34@gmail.com>
2026-09-04 22:31:33 -0600

    fix: Moving .assets again, adding new scriptableobject and warnings gone

---
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/ScriptableObjects/CharacterDefinition/DashProfile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace TDOM.Data
+{
+    [CreateAssetMenu(menuName = "tdom/Dash Profile")]
+    public sealed class DashProfile : ScriptableObject
+    {
+        public float Distance,
+            Duration,
+            Cooldown,
+            MaxTurnRate;
+        public AnimationCurve Easing = AnimationCurve.Linear(0, 0, 1, 1);
+    }
+}
