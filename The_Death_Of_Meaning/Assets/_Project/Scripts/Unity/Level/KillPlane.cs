df5e4c75de37e27558bc6cd2749e5bc556e59a73
Angry6irds <speedlimon34@gmail.com>
2026-09-12 00:38:14 -0600

    feat: Adding graybox

---
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/Unity/Level/KillPlane.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace TDOM.Unity.Level
+{
+    public sealed class KillPlane : MonoBehaviour
+    {
+        [SerializeField] private Transform _respawn;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<PlayerRoot>(out var player)) return;
+            if (!player.IsOwner) return;
+
+            // Evitamos un GetComponent extra
+            if (other.TryGetComponent<CharacterController>(out var cc))
+            {
+                cc.enabled = false;
+                other.transform.position = _respawn.position;
+                cc.enabled = true;
+            }
+        }
+    }
+}
