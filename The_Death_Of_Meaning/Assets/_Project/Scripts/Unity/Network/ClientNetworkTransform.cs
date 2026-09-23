0b16cf777b333bf9a6a5ba8a5056657681e1ea97
VicBojo <reybojorgejr@hotmail.com>
2026-09-23 01:32:10 -0600

    fix: adding network manifest files to replace

--- The_Death_Of_Meaning/Assets/_Project/Scripts/Unity/Network/ClientNetworkTransform.cs
+++ The_Death_Of_Meaning/Assets/_Project/Scripts/Unity/Network/ClientNetworkTransform.cs
@@ -1,4 +1,3 @@
-using Unity.Netcode;
 using Unity.Netcode.Components;
 using UnityEngine;
 
@@ -7,22 +6,6 @@ namespace TDOM.Unity
     [DisallowMultipleComponent]
     public class ClientNetworkTransform : NetworkTransform
     {
-        [ServerRpc]
-        public void RandomTeleportServerRpc()
-        {
-            var oldPosition = transform.position;
-            transform.position = GetRandomPositionOnXYPlane();
-            var newPosition = transform.position;
-            print(
-                $"{nameof(RandomTeleportServerRpc)}() -> {nameof(OwnerClientId)}: {OwnerClientId} --- {nameof(oldPosition)}: {oldPosition} --- {nameof(newPosition)}: {newPosition}"
-            );
-        }
-
-        private static Vector3 GetRandomPositionOnXYPlane()
-        {
-            return new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f);
-        }
-
         protected override bool OnIsServerAuthoritative()
         {
             return false;
